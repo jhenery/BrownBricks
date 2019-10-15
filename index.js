229b4d365c14e03883062b8fa8b168fa0d881bf0
jhenery <54946731+jhenery@users.noreply.github.com>
2019-10-14 21:12:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const express = require("express");
+var express = require('express');
 
-const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("Hello Internet")
-})
+var app = express();
+var server = app.listen(3000, function(){
+    console.log('Listening to requests on port 3000');
+});
 
-app.get("/:param", (req,res)=>{
-    res.send("You requested " + req.params["param"])
-})
+app.use(express.static('public'));
 
-app.use(function(req, res){
-    res.send("Page Not Found")
-})
 
-app.listen(7070, function(err){
-    if(err)return console.log("Could't bromg i[ the server.");
-    console.log("Server is running");
-})
-
-console.log("Hello node");
